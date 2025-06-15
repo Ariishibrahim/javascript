364a8b013c0e17fe390f9641789559b48456c05c
Ariish Ibrahim <ariishibrahim@DESKTOP-F221FBL.>
2025-06-14 20:35:27 -0700

    my javascript work

---
+++ variables_datatypes/script.js
@@ -0,0 +1,16 @@
+var age = 25;
+var  firstNme = "Fadumo";
+var town = "London";
+var nationality = "Swedish";
+var isMarried = true;
+
+
+console.log(firstNme)
+console.log(town)
+console.log(nationality)
+console.log(age)
+console.log(isMarried)
+console.log()
+
+
+
