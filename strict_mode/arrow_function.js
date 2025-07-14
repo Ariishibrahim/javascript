0a7522dd0d54f775ae904c14ca76e33d109825e4
Ariish Ibrahim <katra_abdi@yahoo.com>
2025-07-14 13:53:16 -0700

    learnt strict_mode and arrow_function

---
+++ strict_mode/arrow_function.js
@@ -0,0 +1,24 @@
+// let multiply1= function() {
+//     return 2 * 2;
+// }
+// let result1 = multiply1();    
+// console.log(result1)
+
+
+// let  multiply = (num1, num2) => num1 * num2;
+
+// let result = multiply(5, 10);    
+// console.log(result)
+
+// let add = function() {
+//     return 5 + 5;
+// }
+// let sum = add();
+// console.log(sum)
+
+
+let add = () =>  5 + 5;
+
+let sum = add();
+
+console.log(sum)
